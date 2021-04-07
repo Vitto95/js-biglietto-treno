50475c0ee300e5d48b5a92592982069b1ec258e0
Vitto95 <grimaldi.vitto@gmail.com>
2021-04-07 16:40:39 +0200

    impostazione js su html

--- js/script.js
+++ js/script.js
@@ -18,12 +18,28 @@ const DISC_40 = 0.4;
 if (yourAge >= 18 && yourAge <= 65) {
   ticketPrice = (yourDistance * PRICEFORKM).toFixed(2);
   console.log("Prezzo Biglietto - Tariffa Piena: ", ticketPrice, "euro.");
+  document.getElementById("ticket-price").innerHTML =
+    "Prezzo Biglietto - Tariffa Piena: " + ticketPrice + " euro.";
 } else if (yourAge < 18) {
   ticketPrice = (yourDistance * PRICEFORKM * DISC_20).toFixed(2);
   console.log("Prezzo Biglietto - Tariffa Under 18: ", ticketPrice, "euro.");
+  document.getElementById("ticket-price").innerHTML =
+    "Prezzo Biglietto - Tariffa Under 18: " + ticketPrice + " euro.";
 } else if (yourAge > 65) {
   ticketPrice = (yourDistance * PRICEFORKM * DISC_40).toFixed(2);
   console.log("Prezzo Biglietto - Tariffa Over 65: ", ticketPrice, "euro.");
+  document.getElementById("ticket-price").innerHTML =
+    "Prezzo Biglietto - Tariffa Over 65: " + ticketPrice + " euro.";
 }
 
-console.log("Prezzo Biglietto: ", ticketPrice, "euro.");
+//inizio dati su HTML
+
+//info passeggero
+
+document.getElementById("distance").innerHTML =
+  "Km da percorrere: " + yourDistance + " km";
+
+document.getElementById("age").innerHTML =
+  "Età passeggero: " + yourAge + " anni";
+
+//fine dati su HTML
